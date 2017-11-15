15aded11d4fb0c0b15f86c60ea8f10e69aee697c
Jamie <jamie.lo112@gmail.com>
2017-11-15 15:43:19 -0800

    [add]
    add some video component

--- client/src/components/Sling/index.js
+++ client/src/components/Sling/index.js
@@ -23,7 +23,34 @@ class ProtectedSling extends Component {
   }
 
   openWindow = () => {
-    window.open();
+    'use strict';
+    
+    navigator.getUserMedia = navigator.getUserMedia ||
+        navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+    
+    var constraints = {
+      audio: true,
+      video: true
+    };
+    var video = document.querySelector('video');
+    
+    function successCallback(stream) {
+      window.stream = stream; // stream available to console
+      if (window.URL) {
+        video.src = window.URL.createObjectURL(stream);
+      } else {
+        video.src = stream;
+      }
+    }
+    
+    function errorCallback(error) {
+      console.log('navigator.getUserMedia error: ', error);
+    }
+    
+    navigator.getUserMedia(constraints, successCallback, errorCallback);
+    window.open(
+      <video autoplay></video>
+    );
   }
 
   render() {
